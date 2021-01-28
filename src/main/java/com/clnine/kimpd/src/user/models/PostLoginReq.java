c7b3ec124f47e1db7c0917376e0fd6adff849227
nays111 <43735448+nays111@users.noreply.github.com>
2021-01-29 01:46:51 +0900

    [Add]template upload

---
+++ src/main/java/com/clnine/kimpd/src/user/models/PostLoginReq.java
@@ -0,0 +1,12 @@
+package com.clnine.kimpd.src.user.models;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+public class PostLoginReq {
+    private String email;
+    private String password;
+}
