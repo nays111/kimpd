42583a1b7efee96a5cde4f53fb2493af105c45fb
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-11 05:13:24 +0900

    feat : 마이페이지 조회 API

--- src/main/java/com/clnine/kimpd/src/Web/expert/ExpertController.java
+++ src/main/java/com/clnine/kimpd/src/Web/expert/ExpertController.java
@@ -6,11 +6,15 @@ import com.clnine.kimpd.config.BaseResponseStatus;
 import com.clnine.kimpd.src.Web.expert.models.GetExpertRes;
 import com.clnine.kimpd.src.Web.project.ProjectRepository;
 import com.clnine.kimpd.src.Web.project.models.GetProjectRes;
+import com.clnine.kimpd.src.Web.user.UserInfoProvider;
 import com.clnine.kimpd.src.Web.user.UserInfoRepository;
+import com.clnine.kimpd.src.Web.user.models.GetUsersRes;
 import com.clnine.kimpd.src.Web.user.models.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.clnine.kimpd.config.BaseResponseStatus.SUCCESS;
 
@@ -21,6 +25,7 @@ import static com.clnine.kimpd.config.BaseResponseStatus.SUCCESS;
 @CrossOrigin
 public class ExpertController {
     private final ExpertProvider expertProvider;
+    private final UserInfoProvider userInfoProvider;
 
     /**
      * [2021.02.07] 전문가 상세 조회 API
@@ -34,6 +39,20 @@ public class ExpertController {
             GetExpertRes getExpertRes = expertProvider.getExpertRes(userIdx);
             return new BaseResponse<>(SUCCESS,getExpertRes);
         } catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+    @GetMapping("/experts")
+    public BaseResponse<List<GetUsersRes>> getExperts(@RequestParam(required = false) String word,
+                                                      @RequestParam(required = false) Integer jobCategoryParentIdx,
+                                                      @RequestParam(required = false) Integer jobCategoryChildIdx,
+                                                      @RequestParam(required = false) Integer genreCategoryIdx,
+                                                      @RequestParam(required = false) String city){
+        try{
+            List<GetUsersRes> getUsersResList = userInfoProvider.findExperts(word,jobCategoryParentIdx,jobCategoryChildIdx,genreCategoryIdx,city);
+            return new BaseResponse<>(SUCCESS,getUsersResList);
+        }catch (BaseException exception) {
             return new BaseResponse<>(exception.getStatus());
         }
     }
---
+++ src/main/java/com/clnine/kimpd/src/Web/user/models/GetMyUserInfoRes.java
@@ -0,0 +1,32 @@
+package com.clnine.kimpd.src.Web.user.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class GetMyUserInfoRes {
+    private final int userIdx;
+    private final String profileImagerURL;
+    private final String id;
+    private final String nickname;
+    private final String phoneNum;
+    private final String email;
+    /**
+     * 개인 사업자용
+     */
+    private final String privateBusinessName;
+    private final String businessNumber;
+    private final String businessImageURL;
+
+    /**
+     * 법인 사업자용
+     */
+    private final String corpBusinessName;
+    private final String corpBusinessNumber;
+
+
+
+}
