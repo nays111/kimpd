ad56a18b53d7438f3d917d87ca2c4756978983e1
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-14 19:01:35 +0900

    feat : 평가하기 API

--- src/main/java/com/clnine/kimpd/src/Web/report/ReportController.java
+++ src/main/java/com/clnine/kimpd/src/Web/report/ReportController.java
@@ -3,15 +3,15 @@ package com.clnine.kimpd.src.Web.report;
 import com.clnine.kimpd.config.BaseException;
 import com.clnine.kimpd.config.BaseResponse;
 import com.clnine.kimpd.src.Web.report.models.GetReportCategoryRes;
+import com.clnine.kimpd.src.Web.report.models.PostReportReq;
+import com.clnine.kimpd.utils.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.clnine.kimpd.config.BaseResponseStatus.EMPTY_REPORT_DESCRIPTION;
 import static com.clnine.kimpd.config.BaseResponseStatus.SUCCESS;
 
 @RestController
@@ -19,7 +19,14 @@ import static com.clnine.kimpd.config.BaseResponseStatus.SUCCESS;
 @RequiredArgsConstructor
 public class ReportController {
     private final ReportProvider reportProvider;
+    private final JwtService jwtService;
+    private final ReportService reportService;
 
+    /**
+     * 신고유형 카테고리 조회 API
+     * @return
+     * @throws BaseException
+     */
     @ResponseBody
     @GetMapping("/report-categories")
     public BaseResponse<List<GetReportCategoryRes>> getReportCategories()throws BaseException {
@@ -27,6 +34,38 @@ public class ReportController {
         try{
             getReportCategoryResList = reportProvider.getReportCategory();
             return new BaseResponse<>(SUCCESS,getReportCategoryResList);
+        }catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+    /**
+     * 신고하기 API
+     * @param reportedUserIdx
+     * @param postReportReq
+     * @return
+     */
+    @ResponseBody
+    @PostMapping("/castings/{userIdx}/reports")
+    public BaseResponse<String> postReport(@PathVariable(required = true,value="userIdx")int reportedUserIdx,
+                                           @RequestBody PostReportReq postReportReq){
+        int reporterUserIdx;
+        try{
+            reporterUserIdx = jwtService.getUserIdx();
+        }catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+
+        if(postReportReq.getReportDescription()==null || postReportReq.getReportDescription().length()==0){
+            return new BaseResponse<>(EMPTY_REPORT_DESCRIPTION);
+        }
+
+        try{
+            reportService.postReport(reporterUserIdx,reportedUserIdx,postReportReq);
+
+
+
+            return new BaseResponse<>(SUCCESS);
         }catch(BaseException exception){
             return new BaseResponse<>(exception.getStatus());
         }
--- src/main/java/com/clnine/kimpd/src/Web/report/ReportService.java
+++ src/main/java/com/clnine/kimpd/src/Web/report/ReportService.java
@@ -1,10 +1,65 @@
 package com.clnine.kimpd.src.Web.report;
 
+import com.clnine.kimpd.config.BaseException;
+import com.clnine.kimpd.config.BaseResponseStatus;
+import com.clnine.kimpd.src.Web.report.models.PostReportReq;
+import com.clnine.kimpd.src.Web.report.models.Report;
+import com.clnine.kimpd.src.Web.report.models.ReportCategory;
+import com.clnine.kimpd.src.Web.user.UserInfoRepository;
+import com.clnine.kimpd.src.Web.user.models.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ReportService {
+    private final UserInfoRepository userInfoRepository;
+    private final ReportCategoryRepository reportCategoryRepository;
+    private final ReportRepository reportRepository;
+
+    @Transactional
+    public void postReport(int reportUserIdx, int reportedUserIdx, PostReportReq postReportReq) throws BaseException {
+        UserInfo reportUserInfo;
+        try {
+            reportUserInfo = userInfoRepository.findUserInfoByUserIdxAndStatus(reportUserIdx,"ACTIVE");
+        } catch (Exception ignored) {
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_USER);
+        }
+        UserInfo reportedUserInfo;
+        try {
+            reportedUserInfo = userInfoRepository.findUserInfoByUserIdxAndStatus(reportedUserIdx,"ACTIVE");
+        } catch (Exception ignored) {
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_USER);
+        }
+        int reportCount;
+        try{
+            reportCount = reportRepository.countReportByReportedUserInfoAndStatus(reportedUserInfo,"ACTIVE");
+        }catch (Exception ignored) {
+            throw new BaseException(BaseResponseStatus.FAILED_TO_COUNT_REPORT);
+        }
+        //신고 5회 이상이면
+        if(reportCount >=5){
+            reportedUserInfo.setStatus("INACTIVE");
+            userInfoRepository.save(reportedUserInfo);
+        }
+
+
+        ReportCategory reportCategory;
+        try{
+            reportCategory = reportCategoryRepository.findByReportCategoryIdx(postReportReq.getReportCategoryIdx());
+        }catch (Exception ignored) {
+            throw new BaseException(BaseResponseStatus.FAILED_TO_GET_REPORT_CATEGORIES);
+        }
+        String description = postReportReq.getReportDescription();
+        Report report = new Report(reportUserInfo,reportedUserInfo,reportCategory,description);
+
+        try{
+            reportRepository.save(report);
+        }catch (Exception ignored) {
+            throw new BaseException(BaseResponseStatus.FAILED_TO_POST_REPORT);
+        }
+    }
 
 }
