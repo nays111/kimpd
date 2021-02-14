657df8ff3ecd1bf9718731712946c6216b27cee7
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-15 03:36:31 +0900

    feat : 1:1문의 내역 리스트 조회 API

---
+++ src/main/java/com/clnine/kimpd/src/Web/inquiry/models/GetInquiryListRes.java
@@ -0,0 +1,15 @@
+package com.clnine.kimpd.src.Web.inquiry.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GetInquiryListRes {
+    private final int no;
+    private final int inquiryIdx;
+    private final String answerStatus;
+    private final String inquiryTitle;
+    private final String userNickname;
+    private final String createdDate;
+}
