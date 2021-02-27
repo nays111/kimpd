057ea3c6a32698680751a67f0749f6dfe824b102
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-28 04:20:33 +0900

    feat : 전문가 프로필 관리(조회) API

---
+++ src/main/java/com/clnine/kimpd/src/Web/expert/models/UserGenreCategoryDTO.java
@@ -0,0 +1,11 @@
+package com.clnine.kimpd.src.Web.expert.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserGenreCategoryDTO {
+    private final int genreCategoryIdx;
+    private final String genreCategoryName;
+}
---
+++ src/main/java/com/clnine/kimpd/src/Web/expert/models/UserJobCategoryChildDTO.java
@@ -0,0 +1,11 @@
+package com.clnine.kimpd.src.Web.expert.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserJobCategoryChildDTO {
+    private final int jobCategoryChildIdx;
+    private final String jobCategoryChildName;
+}
