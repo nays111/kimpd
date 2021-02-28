66229dc72c3a2dc32c929b53babb5662a220a3d0
nays111 <43735448+nays111@users.noreply.github.com>
2021-03-01 03:24:21 +0900

    feat : swagger 셋팅

--- src/main/java/com/clnine/kimpd/src/WebSecurityConfig.java
+++ src/main/java/com/clnine/kimpd/src/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.clnine.kimpd.src;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,6 +13,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
     }
+    @Override
+    public void configure(WebSecurity web) {
+        assert web != null;
+        web.ignoring()
+                .antMatchers("/swagger-ui.html");
+    }
+
 }
 
 
