fea3a51a751b7216488b272d5b289717fd3d0bb8
cookie-god <lion0193@gmail.com>
2021-03-01 17:57:51 +0900

    add view all casting and detail casting

--- src/main/java/com/clnine/kimpd/src/WebAdmin/casting/AdminCastingController.java
+++ src/main/java/com/clnine/kimpd/src/WebAdmin/casting/AdminCastingController.java
@@ -1,7 +1,14 @@
 package com.clnine.kimpd.src.WebAdmin.casting;
 
+import com.clnine.kimpd.config.BaseException;
+import com.clnine.kimpd.config.BaseResponse;
+import com.clnine.kimpd.src.WebAdmin.casting.models.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.clnine.kimpd.config.BaseResponseStatus.*;
 
 
 @RestController
@@ -12,46 +19,46 @@ public class AdminCastingController {
     private final AdminCastingService adminCastingService;
 
     /**
-     * 1:1문의 전체 조회 API
-     * [GET] /inquiries
-     * @return BaseResponse<AdminGetInquiriesListRes>
+     * 섭외 전체 조회 API
+     * [GET] /castings
+     * @return BaseResponse<AdminGetCastingsListRes>
      */
-//    @ResponseBody
-//    @GetMapping("/inquiries")
-//    @CrossOrigin(origins = "*")
-//    public BaseResponse<AdminGetInquiriesListRes> getInquiries() throws BaseException{
-//        List<AdminGetInquiriesRes> getInquiriesResList;
-//
-//        try{
-//            getInquiriesResList = adminInquiryProvider.getInquiryList();
-//            AdminGetInquiriesListRes InquiryList = new AdminGetInquiriesListRes(getInquiriesResList);
-//            return new BaseResponse<>(SUCCESS_READ_INQUIRIES, InquiryList);
-//        }catch(BaseException exception){
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
+    @ResponseBody
+    @GetMapping("/castings")
+    @CrossOrigin(origins = "*")
+    public BaseResponse<AdminGetCastingsListRes> getCastings() throws BaseException {
+        List<AdminGetCastingsRes> getInquiriesResList;
+
+        try{
+            getInquiriesResList = adminCastingProvider.getCastingList();
+            AdminGetCastingsListRes castingList = new AdminGetCastingsListRes(getInquiriesResList);
+            return new BaseResponse<>(SUCCESS_READ_INQUIRIES, castingList);
+        }catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
 
     /**
-     * 1:1문의 상세 조회 API
-     * [GET] /inquiries/:inquiryIdx
-     * @PathVariable inquiryIdx
-     * @return BaseResponse<AdminGetNoticesRes>
+     * 섭외 상세 조회 API
+     * [GET] /castings/:castingIdx
+     * @PathVariable castingIdx
+     * @return BaseResponse<AdminGetCastingRes>
      */
-//    @ResponseBody
-//    @GetMapping("/inquiries/{inquiryIdx}")
-//    @CrossOrigin(origins = "*")
-//    public BaseResponse<AdminGetInquiryRes> getInquiry(@PathVariable Integer inquiryIdx) {
-//        if (inquiryIdx == null || inquiryIdx <= 0) {
-//            return new BaseResponse<>(EMPTY_INQUIRY_IDX);
-//        }
-//
-//        try {
-//            AdminGetInquiryRes adminGetInquiryRes = adminInquiryProvider.retrieveInquiryInfo(inquiryIdx);
-//            return new BaseResponse<>(SUCCESS_READ_INQUIRIES, adminGetInquiryRes);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
+    @ResponseBody
+    @GetMapping("/castings/{castingIdx}")
+    @CrossOrigin(origins = "*")
+    public BaseResponse<AdminGetCastingRes> getCasting(@PathVariable Integer castingIdx) {
+        if (castingIdx == null || castingIdx <= 0) {
+            return new BaseResponse<>(EMPTY_INQUIRY_IDX);
+        }
+
+        try {
+            AdminGetCastingRes adminGetCastingRes = adminCastingProvider.retrieveCastingInfo(castingIdx);
+            return new BaseResponse<>(SUCCESS_READ_INQUIRIES, adminGetCastingRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
 
     /**
      * 1:1문의 답글 API
--- src/main/java/com/clnine/kimpd/src/WebAdmin/casting/AdminCastingProvider.java
+++ src/main/java/com/clnine/kimpd/src/WebAdmin/casting/AdminCastingProvider.java
@@ -1,11 +1,16 @@
 package com.clnine.kimpd.src.WebAdmin.casting;
 
 import com.clnine.kimpd.config.BaseException;
-import com.clnine.kimpd.src.WebAdmin.casting.models.AdminCasting;
+import com.clnine.kimpd.src.WebAdmin.casting.models.*;
+import com.clnine.kimpd.src.WebAdmin.user.models.AdminUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.clnine.kimpd.config.BaseResponseStatus.FAILED_TO_GET_INQUIRIES;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.clnine.kimpd.config.BaseResponseStatus.*;
 
 @Service
 public class AdminCastingProvider {
@@ -19,84 +24,95 @@ public class AdminCastingProvider {
     }
 
     /**
-     * Inquiry 전체 조회
-     * @return List<AdminGetInquiriesRes>
+     * casting 전체 조회
+     * @return List<AdminGetCastingsRes>
      * @throws BaseException
      */
-//    public List<AdminGetInquiriesRes> getInquiryList() throws BaseException{
-//        List<AdminCasting> inquiryList;
-//        try{
-//            inquiryList = adminInquiryRepository.findAll();
-//        }catch(Exception ignored){
-//            throw new BaseException(FAILED_TO_GET_FAQS);
-//        }
-//
-//        SimpleDateFormat sDate = new SimpleDateFormat("yyyy-MM-dd");
-//        return inquiryList.stream().map(Inquiry ->{
-//            int inquiryIdx = Inquiry.getInquiryIdx();
-//            String inquiryAnswer = Inquiry.getInquiryAnswer();
-//            String answerState = null;
-//            if(inquiryAnswer == null || inquiryAnswer.length() == 0){
-//                answerState = "답변대기";
-//            }
-//            else{
-//                answerState = "답변완료";
-//            }
-//            String inquiryTitle = Inquiry.getInquiryTitle();
-//            String nickname = Inquiry.getAdminUserInfo().getNickname();
-//            String createdAt = sDate.format(Inquiry.getCreatedAt());
-//            String status = Inquiry.getStatus();
-//            return new AdminGetInquiriesRes(inquiryIdx, answerState, inquiryTitle, nickname, createdAt, status);
-//        }).collect(Collectors.toList());
-//    }
+    public List<AdminGetCastingsRes> getCastingList() throws BaseException{
+        List<AdminCasting> castingList;
+        try{
+            castingList = adminCastingRepository.findAll();
+        }catch(Exception ignored){
+            throw new BaseException(FAILED_TO_GET_FAQS);
+        }
+
+        return castingList.stream().map(Casting ->{
+            int castingIdx = Casting.getCastingIdx();
+            String userNickname = Casting.getAdminUserInfo().getNickname();
+            String expertNickname = Casting.getAdminExpertInfo().getNickname();
+            String projectName = Casting.getAdminProject().getProjectName();
+            String castingWork = Casting.getCastingWork();
+            String castingStatus = null;
+            if(Casting.getCastingStatus() == 1){
+                castingStatus = "섭외중";
+            }
+            else if(Casting.getCastingStatus() == 2){
+                castingStatus = "섭외완료";
+            }
+            else if(Casting.getCastingStatus() == 3){
+                castingStatus = "섭외거절";
+            }
+            else if(Casting.getCastingStatus() == 4){
+                castingStatus = "프로젝트 완료";
+            }
+            String status = Casting.getStatus();
+            return new AdminGetCastingsRes(castingIdx, userNickname, expertNickname, projectName, castingWork, castingStatus, status);
+        }).collect(Collectors.toList());
+    }
 
     /**
-     * 1:1문의 상세 조회
-     * @param inquiryIdx
-     * @return AdminGetInquiryRes
+     * Casting 상세 조회
+     * @param castingIdx
+     * @return AdminGetCastingRes
      * @throws BaseException
      */
-//    public AdminGetInquiryRes retrieveInquiryInfo(int inquiryIdx) throws BaseException {
-//        // 1. DB에서 inquiryIdx AdminInquiry 조회
-//        AdminCasting adminCasting = retrieveInquiryByInquiryIdx(inquiryIdx);
-//
-//        if(adminCasting == null){
-//            throw new BaseException(FAILED_TO_GET_INQUIRIES);
-//        }
-//
-//        ArrayList<AdminInquiryFile> adminInquiryFileList = adminInquiryFileRepository.findAllByAdminInquiry(adminCasting);
-//
-//        if(adminInquiryFileList == null){
-//            throw new BaseException(FAILED_TO_GET_INQUIRIES);
-//        }
-//
-//        ArrayList<String> fileList = new ArrayList<>();
-//        for(int i = 0; i < adminInquiryFileList.size(); i++){
-//            fileList.add(i, adminInquiryFileList.get(i).getInquiryFileName());
-//        }
-//
-//        // 2. AdminGetInquiryRes 변환하여 return
-//        return new AdminGetInquiryRes(adminCasting.getAdminInquiryCategory().getInquiryCategoryName(),
-//                adminCasting.getInquiryTitle(), adminCasting.getAdminUserInfo().getNickname(), adminCasting.getInquiryDescription(),
-//                adminCasting.getInquiryAnswer(), fileList, adminCasting.getStatus());
-//    }
+    public AdminGetCastingRes retrieveCastingInfo(int castingIdx) throws BaseException {
+        // 1. DB에서 castingIdx AdminCasting 조회
+        AdminCasting adminCasting = retrieveCastingByCastingIdx(castingIdx);
+
+        if(adminCasting == null){
+            throw new BaseException(FAILED_TO_GET_INQUIRIES);
+        }
+
+        String castingStatus = null;
+        if(adminCasting.getCastingStatus() == 1){
+            castingStatus = "섭외중";
+        }
+        else if(adminCasting.getCastingStatus() == 2){
+            castingStatus = "섭외완료";
+        }
+        else if(adminCasting.getCastingStatus() == 3){
+            castingStatus = "섭외거절";
+        }
+        else if(adminCasting.getCastingStatus() == 4){
+            castingStatus = "프로젝트 완료";
+        }
+
+        // 2. AdminGetCastingRes 변환하여 return
+        return new AdminGetCastingRes(adminCasting.getAdminProject().getProjectName(), adminCasting.getAdminProject().getProjectMaker(),
+                adminCasting.getAdminProject().getProjectManager(), adminCasting.getAdminProject().getProjectDescription(),
+                adminCasting.getAdminProject().getProjectFileURL(), adminCasting.getAdminUserInfo().getNickname(),
+                adminCasting.getAdminExpertInfo().getNickname(), adminCasting.getCastingPrice(), adminCasting.getCastingPriceDate(),
+                adminCasting.getCastingMessage(), adminCasting.getCastingStartDate(), adminCasting.getCastingEndDate(), adminCasting.getCastingWork(),
+                castingStatus, adminCasting.getStatus());
+    }
 
     /**
-     * Inquiry 조회
-     * @param inquiryIdx
-     * @return AdminInquiry
+     * Casting 조회
+     * @param castingIdx
+     * @return AdminCasting
      * @throws BaseException
      */
-    public AdminCasting retrieveInquiryByInquiryIdx(int inquiryIdx) throws BaseException {
-        // 1. DB에서 AdminInquiry 조회
+    public AdminCasting retrieveCastingByCastingIdx(int castingIdx) throws BaseException {
+        // 1. DB에서 AdminCasting 조회
         AdminCasting adminCasting;
         try {
-            adminCasting = adminCastingRepository.findById(inquiryIdx).orElse(null);
+            adminCasting = adminCastingRepository.findById(castingIdx).orElse(null);
         } catch (Exception ignored) {
             throw new BaseException(FAILED_TO_GET_INQUIRIES);
         }
 
-        // 2. AdminInquiry return
+        // 2. AdminCasting return
         return adminCasting;
     }
 }
