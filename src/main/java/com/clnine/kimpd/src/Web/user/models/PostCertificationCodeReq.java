df5598ca7e657b27b8e58a5f70b06ae1bdc73cf9
nays111 <43735448+nays111@users.noreply.github.com>
2021-03-02 23:12:49 +0900

    fix : 휴대폰 인증 API (POST->GET), 휴대폰 검증 API (GET->POST)

--- src/main/java/com/clnine/kimpd/src/Web/user/models/PostCertificationCodeReq.java
+++ src/main/java/com/clnine/kimpd/src/Web/user/models/PostCertificationCodeReq.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class GetCertificationCodeReq {
+public class PostCertificationCodeReq {
+    private String phoneNum;
     private  Integer code;
 }
