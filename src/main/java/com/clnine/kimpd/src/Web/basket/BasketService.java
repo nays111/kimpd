0a750ec4b5ee9b18d8e986084a8a92fac2635232
nays111 <43735448+nays111@users.noreply.github.com>
2021-03-04 00:20:03 +0900

    refactor : 장바구니 관련 API validation 추가

--- src/main/java/com/clnine/kimpd/src/Web/basket/BasketService.java
+++ src/main/java/com/clnine/kimpd/src/Web/basket/BasketService.java
@@ -23,17 +23,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BasketService {
 
-    private final UserInfoRepository userInfoRepository;
-    private final ProjectRepository projectRepository;
     private final CastingRepository castingRepository;
     private final UserInfoProvider userInfoProvider;
     private final ProjectProvider projectProvider;
 
     /**
-     * 장바구니 담기 함수
-     * @param userIdx
-     * @param postBasketReq
-     * @throws BaseException
+     * 장바구니 담기
      */
     @Transactional
     public void postBasket(int userIdx, PostBasketReq postBasketReq) throws BaseException{
@@ -56,9 +51,8 @@ public class BasketService {
 
     /**
      * 장바구니에 있는 걸로 섭외 요청하기
-     * @param postBasketCastingReq
-     * @throws BaseException
      */
+    @Transactional
     public void postBasketCasting(PostBasketCastingReq postBasketCastingReq) throws BaseException{
         List<Integer> castingIdxList = postBasketCastingReq.getCastingIdx();
         List<Casting> castingList = new ArrayList<>();
@@ -69,13 +63,15 @@ public class BasketService {
             }catch (Exception ignored){
                 throw new BaseException(BaseResponseStatus.NOT_FOUND_CASTING);
             }
+            if(casting==null){
+                throw new BaseException(BaseResponseStatus.NOT_FOUND_CASTING);
+            }
             String castingStartDate = casting.getCastingStartDate();
             String castingEndDate = casting.getCastingEndDate();
             String castingPrice = casting.getCastingPrice();
             String castingMessage = casting.getCastingMessage();
             String castingWork = casting.getCastingWork();
             String castingPriceDate = casting.getCastingPriceDate();
-
             if(castingMessage==null || castingWork==null || castingPriceDate==null || castingStartDate==null || castingEndDate==null || castingPrice==null){
                 throw new BaseException(BaseResponseStatus.DID_NOT_INSERT_CASTING_CONDITION);
             }else {
@@ -86,8 +82,11 @@ public class BasketService {
         for(int i=0;i<castingList.size();i++) {
             Casting casting = castingList.get(i);
             casting.setCastingStatus(1);
-            castingRepository.save(casting);
+            try{
+                castingRepository.save(casting);
+            }catch (Exception ignored){
+                throw new BaseException(BaseResponseStatus.FAILED_TO_POST_CASTING);
+            }
         }
     }
-
 }
